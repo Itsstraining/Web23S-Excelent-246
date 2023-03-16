e3942c76421455a106ff1b60a2e1ce53a5f8b185
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2023-03-16 09:56:59 +0700

    fix

--- client/src/app/env/environment.prod.ts
+++ client/src/app/env/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
-    production: true,
-    firebase: {
-      apiKey: "AIzaSyBjW9srOkcYwOdpW5kwj0VTEevRhsjfpiA",
-      authDomain: "excelmentos.firebaseapp.com",
-      projectId: "excelmentos",
-      storageBucket: "excelmentos.appspot.com",
-      messagingSenderId: "123965683087",
-      appId: "1:123965683087:web:20cfaaf85c908c269747fa"
-    },
-    baseUrl: 'http://localhost:3000/',
-  };
-  
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBjW9srOkcYwOdpW5kwj0VTEevRhsjfpiA",
+    authDomain: "excelmentos.firebaseapp.com",
+    projectId: "excelmentos",
+    storageBucket: "excelmentos.appspot.com",
+    messagingSenderId: "123965683087",
+    appId: "1:123965683087:web:20cfaaf85c908c269747fa"
+  },
+  baseUrl: 'https://excelent-k32n4ohtya-uc.a.run.app/',
+};
