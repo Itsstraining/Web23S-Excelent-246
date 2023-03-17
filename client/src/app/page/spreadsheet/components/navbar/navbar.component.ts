2dfaf47ab0f03cb73980c92fbf783b0e17878358
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2023-03-18 00:27:29 +0700

    close project 18/3/2023

--- client/src/app/page/spreadsheet/components/navbar/navbar.component.ts
+++ client/src/app/page/spreadsheet/components/navbar/navbar.component.ts
@@ -129,7 +129,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   menuItemText: string[] = [
-    'Open',
     'Save',
     'Exit',
     'Cut',
@@ -154,10 +153,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
     {
       text: 'File',
       items: [
-        { text: 'Open', },
         { text: 'Save', },
-        { text: 'Exit', },
-
       ],
     },
     {
